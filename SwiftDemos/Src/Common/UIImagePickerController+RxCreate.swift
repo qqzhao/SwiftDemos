ae89c5b14748f6515ad7c9fb3b45a75b5500f039
qianzhao <qianzhao@tencent.com>
2018-04-23 21:10:29 +0800

    update Common

---
+++ SwiftDemos/Src/Common/UIImagePickerController+RxCreate.swift
@@ -0,0 +1,91 @@
+//
+//  UIImagePickerController+RxCreate.swift
+//  SwiftDemos
+//
+//  Created by qianzhao on 2018/4/23.
+//  Copyright © 2018年 qianzhao. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+func dismissViewController(_ viewController: UIViewController, animated: Bool) {
+    if viewController.isBeingDismissed || viewController.isBeingPresented {
+        DispatchQueue.main.async {
+            dismissViewController(viewController, animated: animated)
+        }
+        
+        return
+    }
+    
+    if viewController.presentingViewController != nil {
+        viewController.dismiss(animated: animated, completion: nil)
+    }
+}
+
+extension Reactive where Base: UIImagePickerController {
+    static func createWithParent(_ parent: UIViewController?, animated: Bool = true, configureImagePicker: @escaping (UIImagePickerController) throws -> () = { x in }) -> Observable<UIImagePickerController> {
+        return Observable.create { [weak parent] observer in
+            let imagePicker = UIImagePickerController()
+            let dismissDisposable = imagePicker.rx
+                .didCancel
+                .subscribe(onNext: { [weak imagePicker] _ in
+                    print("weak")
+                    guard let imagePicker = imagePicker else {
+                        return
+                    }
+                    dismissViewController(imagePicker, animated: animated)
+                })
+            
+            do {
+                try configureImagePicker(imagePicker)
+            }
+            catch let error {
+                observer.on(.error(error))
+                return Disposables.create()
+            }
+            
+            guard let parent = parent else {
+                observer.on(.completed)
+                return Disposables.create()
+            }
+            
+            parent.present(imagePicker, animated: animated, completion: nil)
+            observer.on(.next(imagePicker))
+            
+            return Disposables.create(dismissDisposable, Disposables.create {
+                dismissViewController(imagePicker, animated: animated)
+            })
+        }
+    }
+    
+    /**
+     Reactive wrapper for `delegate` message.
+     */
+    public var didFinishPickingMediaWithInfo: Observable<[String : AnyObject]> {
+        return delegate
+            .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:)))
+            .map({ (a) in
+                return try castOrThrow(Dictionary<String, AnyObject>.self, a[1])
+            })
+    }
+    
+    /**
+     Reactive wrapper for `delegate` message.
+     */
+    public var didCancel: Observable<()> {
+        return delegate
+            .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerControllerDidCancel(_:)))
+            .map {_ in () }
+    }
+}
+
+fileprivate func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
+    guard let returnValue = object as? T else {
+        throw RxCocoaError.castingError(object: object, targetType: resultType)
+    }
+    
+    return returnValue
+}
